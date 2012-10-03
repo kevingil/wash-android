3ef00ba6c7b25c44e4f7b222ab9a7ac399e47ff5
Kevin Gil <keven.gs@gmail.com>
2012-10-03 15:27:30 -0700

    Daily Bulletin Fixes
    -Added more js to only show the daily bulletin
    -more style changes
    -App to be published next week

--- src/com/kevingil/gwhs/DailyBulletin.java
+++ src/com/kevingil/gwhs/DailyBulletin.java
@@ -5,15 +5,11 @@ import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
 
 import android.app.ProgressDialog;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.KeyEvent;
-import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
-import android.webkit.WebSettings.ZoomDensity;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -27,6 +23,7 @@ public class DailyBulletin extends SherlockActivity {
     WebView myWebView;
 	ProgressBar progressBar;
 	WebSettings WebViewSettings;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	   if (Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH){
@@ -38,29 +35,39 @@ public class DailyBulletin extends SherlockActivity {
        //  getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.drawable.black_textured));
         // this loads the web view 
         myWebView = (WebView) findViewById(R.id.webviewDailyBulletin);
+        myWebView.loadUrl("https://gwhs-sfusd-ca.schoolloop.com/bulletin");
         final ProgressDialog webViewProgress = ProgressDialog.show(this, "", "your phone's slow lol", true);
-        myWebView.setBackgroundResource(R.drawable.dark_gray_bg);
-        myWebView.setBackgroundColor(0);
-        myWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
-        // settings 
-        WebViewSettings = myWebView.getSettings();
-        WebViewSettings.setLoadsImagesAutomatically(false);
-        WebViewSettings.setUseWideViewPort(true);
-        WebViewSettings.setLoadWithOverviewMode(true);
-        WebViewSettings.setSavePassword(true);
-        WebViewSettings.setSaveFormData(true);
-        WebViewSettings.setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new WebViewClient(){
     	   @Override
     	   public void onPageFinished(WebView view, String url){
+    		   myWebView.loadUrl("javascript:" +
+    		   		"document.getElementById('container_footer').style.display=\"none\";" +
+    		   		"document.getElementById('container_header').style.display=\"none\";" +
+    		   		"document.getElementById('block_standard_left').style.display=\"none\";" +
+    		   		"document.getElementById('block_wide_main').style.backgroundcolor=\"white\";" +
+    		   		"document.getElementById('block_wide_main').style.width=\"50%\";" +
+    		   		"document.getElementById('block_wide_main').style.text-align=\"center\";" +
+    		   		"document.getElementById('block_wide_main').style.margin-left=\"auto\";" +
+    		   		"document.getElementById('block_wide_main').style.margin-right=\"auto\";" +
+    		   		"document.getElementById('body').style.margin=\"0px\";" +
+    		   		"document.getElementsByClass('block_text').style.width=\"1000px\";" +
+    		   		"document.getElementsByClass('block_content_main').style.width=\"100%\";"
+    		   		);
     		   webViewProgress.dismiss();
-    		   myWebView.loadUrl(
-    		   "javascript:(function() { document.getElementsByTagName('body')[0].style.color = 'red';})()"
-    		   );
-    	    }
-    	   
+    	    }//block_content_main
+
        });
-       myWebView.loadUrl("https://gwhs-sfusd-ca.schoolloop.com/bulletin");
+       myWebView.setBackgroundResource(R.drawable.dark_gray_bg);
+       myWebView.setBackgroundColor(0);
+       myWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+       WebViewSettings = myWebView.getSettings();
+       //WebViewSettings.setLoadsImagesAutomatically(false);
+       WebViewSettings.setUseWideViewPort(true);
+       WebViewSettings.setLoadWithOverviewMode(true);
+       WebViewSettings.setSavePassword(true);
+       WebViewSettings.setSaveFormData(true);
+       WebViewSettings.setJavaScriptEnabled(true);
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
--- src/com/kevingil/gwhs/Home.java
+++ src/com/kevingil/gwhs/Home.java
@@ -2,22 +2,28 @@ package com.kevingil.gwhs;
 
 
 
+
 import com.actionbarsherlock.app.SherlockActivity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class Home extends SherlockActivity  {
     WebView myWebView;
 	ProgressBar progressBar;
 	WebSettings WebViewSettings;
+	TextView titleTV;
 	
     public void onCreate(Bundle savedInstanceState) {
     	if (Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH){
@@ -54,6 +60,21 @@ public class Home extends SherlockActivity  {
                     startActivity(intent);
             }
     });
+        /*
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View customView = inflater.inflate(R.layout.custom_text, null);
+
+        titleTV = (TextView) customView.findViewById(R.id.action_custom_title);
+        titleTV.setGravity(Gravity.CENTER);
+        // you can apply a custom typeface here or do sth else...
+
+        getSupportActionBar().setCustomView(customView);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        */
+        
+
     }
     @Override // this is to use go back buttom to navigate in webview history, to exit the webview people will use the back button
     public boolean onKeyDown(int keyCode, KeyEvent event) {
